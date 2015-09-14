68e57628f73d2caee2f2913fdab09c7c79390ae4
Leonhardt Koepsell <leonhardt@aphelion.io>
2015-09-14 14:02:27 -0400

    [#103318808] use ui-router

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,17 @@ require('angular');
 window.jQuery = require('jquery');
 require('bootstrap-sass');
 
-var app = angular.module('lda', []);
+var app = angular.module('lda', [
+    require('angular-ui-router')
+]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state('home', {
+            url: '/',
+            template: '<about-lda/>'
+        });
+});
 
 require('./about_lda_directive');
